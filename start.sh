78c944ecf8d540aeb01b3032c6533cc99e5febe3
ahmedmagdyy <ahmedmagdy2016@gmail.com>
2019-03-08 17:29:53 +0200

    delete

--- start.sh
+++ start.sh
@@ -4,46 +4,46 @@ source ./createDB.sh
 
 
 if [[ ! -d ./databases ]]
-  then
-mkdir ./databases
+then
+    mkdir ./databases
 fi
 
 
 while true
 do
-echo "========================"
-echo "1-create new database"
-echo "2-use database"
-echo "3-drop datbase"
-echo "0-exit"
-
-read -p "enter your choice: " choice
-
-case $choice in
-1)
-  createDB
-  ;;
-
-2)
-  if [[ $(ls ./databases ) != "" ]]
-    then
-  useDB
-else
-  echo "there is no databases"
-fi
-  ;;
-3)
-dropDB
-;;
-
-0)
-  break
-  ;;
-
-*)
-  echo wrong entry
-  ;;
-esac
+    echo "========================"
+    echo "1-create new database"
+    echo "2-use database"
+    echo "3-drop datbase"
+    echo "0-exit"
+    
+    read -p "enter your choice: " choice
+    
+    case $choice in
+        1)
+            createDB
+        ;;
+        
+        2)
+            if [[ $(ls ./databases ) != "" ]]
+            then
+                useDB
+            else
+                echo "there is no databases"
+            fi
+        ;;
+        3)
+            dropDB
+        ;;
+        
+        0)
+            break
+        ;;
+        
+        *)
+            echo wrong entry
+        ;;
+    esac
 done
 
 # createDB
